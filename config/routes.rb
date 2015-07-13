84d46dbae13a7ebf682ade7fc416e1c448f02bdb
Michael Dennis <michaelsdennis4@gmail.com>
2015-07-13 15:54:04 -0400

    created about and login pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get '/about' => 'welcome#about'
+
+  get '/login' => 'welcome#login'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
